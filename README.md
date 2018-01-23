1b45ff73f1b8cb75bd49d3161c8ff2954b26d396
TurkiAlqasem <Turkiqsm>
2018-01-23 06:00:51 +0300

    arcade

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 #arcade-game
 in This game you want to avoid bugs to go to the river.
 you get 1 score each time the player arrive to the river, every time the score reach 5 or 10 or 15,the player will change.
+using left or right keys from the keyboard to move left or right.
+and the same for up and down.
 
 ##how to start
-just open on **index.html** file
+just open on **index.html** file.
+
 
 ##at the End 
 **Enjoy!!**
--- js/app.js
+++ js/app.js
@@ -1,48 +1,38 @@
-
 //Collision variable to check it
-      var Collisioned =false ;
+var Collisioned = false;
 
-            //resets the player and the score
-          var reset = function() {
+//resets the player and the score
 
-        player.x = 200;
-        player.y = 400;
-
-        player.score = 0;
-ScoreHTML();
-
-
-      };
 
 
 //adding or altering the score on the screen
-        var ScoreHTML = function() {
+var ScoreHTML = function() {
 
     $(".Score").html("<p>Score : <b>" + player.score + "</b></p>");
-  }
+}
 // generate random number not bigger then x
-  var random = function(x) {
-  return Math.floor((Math.random() * 10) % x);
+var random = function(x) {
+    return Math.floor((Math.random() * 10) % x);
 }
 
 // Enemies our player must avoid
 
 var Enemy = function() {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
-  this.speed = [100, 80, 150, 130, 120];
-  this.indSpeed = random(5);
+    this.speed = [100, 80, 150, 130, 120];
+    this.indSpeed = random(5);
 
-  this.x = -200;
+    this.x = -200;
 
-  this.ylocations = [60, 140, 220];
-  this.indY = random(3);
-  this.y = this.ylocations[this.indY];
+    this.ylocations = [60, 140, 220];
+    this.indY = random(3);
+    this.y = this.ylocations[this.indY];
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
@@ -51,17 +41,17 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if(this.x>505){
-      this.x=-50;
-      this.y=this.ylocations[random(3)] ;
+    if (this.x > 505) {
+        this.x = -50;
+        this.y = this.ylocations[random(3)];
     }
 
-  this.x = this.x+(dt *this.speed[this.indSpeed]) ;
+    this.x = this.x + (dt * this.speed[this.indSpeed]);
 
-  if (((player.x > Math.floor(this.x-50))&&(player.x < Math.floor(this.x+50))) && ((player.y > this.y-50) && (player.y <this.y+50))){
-    Collisioned = true;
+    if (((player.x > Math.floor(this.x - 50)) && (player.x < Math.floor(this.x + 50))) && ((player.y > this.y - 50) && (player.y < this.y + 50))) {
+        Collisioned = true;
 
-}
+    }
 
 
 };
@@ -72,19 +62,19 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-var checkCollisions = function() {
-if(Collisioned == true){
-  reset();
-  Collisioned=false;
-}
+Enemy.prototype.checkCollisions = function() {
+    if (Collisioned == true) {
+        player.reset();
+        Collisioned = false;
+    }
 
 };
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-  var player = function() {
-    this.x =200;
-    this.y =400;
+var player = function() {
+    this.x = 200;
+    this.y = 400;
     this.score = 0;
 
     this.sprite = 'images/char-boy.png';
@@ -96,55 +86,67 @@ player.prototype.update = function() {
     // which will ensure the game runs at the same speed for
     // all computers.
 
-     if(this.y<-1){
-     this.y=400;
-     this.score++;
-ScoreHTML();
-   }
-
-
 
 
 
 };
+
+
 player.prototype.render = function() {
 
-        if(this.score>=5){
+    if (this.score >= 5) {
 
-          this.sprite='images/char-cat-girl.png';
-        } if(this.score>=10){
-          this.sprite='images/char-horn-girl.png';
+        this.sprite = 'images/char-cat-girl.png';
+    }
+    if (this.score >= 10) {
+        this.sprite = 'images/char-horn-girl.png';
 
-        }if(this.score>=15){
-          this.sprite='images/char-pink-girl.png';
+    }
+    if (this.score >= 15) {
+        this.sprite = 'images/char-pink-girl.png';
 
-        }
+    }
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-player.prototype.handleInput = function(key){
 
-      if (key == 'left' && !(this.x-90<0)) {
+player.prototype.reset = function() {
 
-        this.x-=90;
-      } else if (key == 'right'&& !(this.x+90>405)) {
-          this.x+=90;
-        } else if (key == 'up'/*&& !(this.y-80<0)*/){
-            this.y-=90;
-          }else if (key == 'down'&& !(this.y+90>405)) {
-              this.y+=90;
-}
+    player.x = 200;
+    player.y = 400;
+
+    player.score = 0;
+    ScoreHTML();
+
+
+};
+player.prototype.handleInput = function(key) {
+    if (this.y < 100) {
+        this.y = 400;
+        this.score++;
+        ScoreHTML();
+    }
+    if (key == 'left' && !(this.x - 90 < 0)) {
+
+        this.x -= 90;
+    } else if (key == 'right' && !(this.x + 90 > 405)) {
+        this.x += 90;
+    } else if (key == 'up' /*&& !(this.y-80<0)*/ ) {
+        this.y -= 90;
+    } else if (key == 'down' && !(this.y + 90 > 405)) {
+        this.y += 90;
+    }
 };
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-          var allEnemies =[];
+var allEnemies = [];
 
-            for (var i =5;i>0;i--){
-              allEnemies.push(new Enemy() );
-              }
+for (var i = 5; i > 0; i--) {
+    allEnemies.push(new Enemy());
+}
 
 
-          var player = new player();
+var player = new player();
 
 
 
